48c6212bf5e90c18ff8f510904acbee26b804c64
marknemenzo <mlnemenzo@gmail.com>
2018-05-02 18:30:41 -0700

    completed to do list

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,45 @@
 import 'materialize-css/dist/css/materialize.min.css';
-import React from 'react';
+import React, { Component } from 'react';
 import '../assets/css/app.css';
 import List from './list';
+import AddItem from './add-items';
+import listData from '../helpers/list_data'
 
-const App = () => (
-    <div className = "container">
-        <h1 className = "center">To Do List</h1>
-        <List/>
-    </div>
-);
+class App extends Component {
+
+    constructor(props) {
+        super(props) 
+        
+        this.state = {
+            listData: []
+        };
+    }
+
+    componentDidMount() {
+        this.getListData();
+    }
+
+    AddItem(item) {
+        this.setState({
+            listData: [...this.state.listData, item]
+        });
+    }
+
+    getListData() {
+        setTimeout(() => {
+        this.setState({ listData });
+        }, 500);
+    }
+
+    render() { 
+        return (<div className = "container">
+                    <h1 className = "center">To Do List</h1>
+                    <AddItem add={this.AddItem.bind(this)}/>
+                    <List data = {this.state.listData}/>
+                </div>
+        );
+    }
+
+}
 
 export default App;
